c0c2bfd93cbca96c9d54244059b010e80d1e8509
Rob Dusing <55949742+DuZii56@users.noreply.github.com>
2022-12-22 14:23:43 -0500

    Create jar.py

---
+++ PSET 8/jar/jar.py
@@ -0,0 +1,35 @@
+class Jar:
+    def __init__(self, capacity=12):
+        if capacity >= 0:
+            self._capacity = capacity
+            self._size = 0
+        else:
+            raise ValueError
+
+
+    def __str__(self):
+        return "🍪" * self.size
+
+
+    def deposit(self, n):
+        if self.size + n > self.capacity:
+            raise ValueError
+        else:
+            self._size += n
+
+
+    def withdraw(self, n):
+        if self.size - n < 0:
+            raise ValueError
+        else:
+            self._size -= n
+
+
+    @property
+    def capacity(self):
+        return self._capacity
+
+
+    @property
+    def size(self):
+        return self._size
