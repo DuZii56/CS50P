f85744b1246d3402dc2183145edb399c3be59daa
Rob Dusing <55949742+DuZii56@users.noreply.github.com>
2022-12-22 11:46:43 -0500

    Delete PSET 1/deep directory

--- PSET 1/deep/deep.py
+++
@@ -1,6 +0,0 @@
-meaning = input(str("What is the answer to the Great Question of Life, the Universe and Everything? ")).lower().replace(" ", "").replace("-", "")
-
-if meaning == '42' or meaning == 'fortytwo':
-    print('Yes')
-else:
-    print('No')
